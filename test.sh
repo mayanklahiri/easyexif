6127e6b77390a582d7dfb19049101f0428bcb018
Mayank Lahiri <lahiri@google.com>
2014-12-03 12:41:03 -0800

    Initial port from Google Code.

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+make
+
+for jpeg in `ls test-images/*.jpg`; do
+  ./demo $jpeg > /tmp/`basename $jpeg`.actual
+  diff $jpeg.expected /tmp/`basename $jpeg`.actual > /tmp/diff.out
+  if [[ -s /tmp/diff.out ]] ; then
+    echo "FAILED ON $jpeg"
+    cat /tmp/diff.out
+    exit
+  fi ;
+  echo "PASS $jpeg"
+done
