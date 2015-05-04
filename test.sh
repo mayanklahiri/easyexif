a827a91a650c8c08f85607d00f02add955dd67a8
Tomas Paladin Volf <paladin@jstation.cz>
2015-05-04 10:35:35 +0200

    Fixed tests, test.sh now passes
    1) converted test-images/short-ascii-{II,MM}-.jpg.expected to use
       \n, it was \r\n
    2) updated test-images/*.exepected to match actual result
       from ./demo, see diff for details
    3) extended test.sh to work on windows under cygwin - test.sh looks
       if demo.exe does exist by any chance

--- test.sh
+++ test.sh
@@ -2,8 +2,20 @@
 
 make
 
+DEMONAME=./demo
+
+if [ ! -e ./demo ]; then
+  # check if demo.exe exists, needed on windows
+  if [ -e ./demo.exe ]; then
+    DEMONAME=./demo.exe
+  else
+    echo Cannot find executable.
+    exit 1
+  fi
+fi
+
 for jpeg in `ls test-images/*.jpg`; do
-  ./demo $jpeg > /tmp/`basename $jpeg`.actual
+  $DEMONAME $jpeg > /tmp/`basename $jpeg`.actual
   diff $jpeg.expected /tmp/`basename $jpeg`.actual > /tmp/diff.out
   if [[ -s /tmp/diff.out ]] ; then
     echo "FAILED ON $jpeg"
